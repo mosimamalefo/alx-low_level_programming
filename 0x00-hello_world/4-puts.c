ae64964985ff3450e71a05b58d669a6fbfa28dd9
myUsername <myusername>
2023-02-16 05:25:10 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Successs)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingul puzzel");
+	return (0);
