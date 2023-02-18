f6cb3ef18a52e527cdbfb623a7f9597965b7157e
myUsername <myusername>
2023-02-17 18:19:47 -0800

    print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,10 @@ int main(void)
 
 	while (d < 10)
 	{
-		c == 0;
+		c = 0;
 		while (c < 10)
 		{
-			if (d != c && d < 10)
+			if (d != c && d < c)
 			{
 				putchar('0' + d);
 				putchar('0' + c);
