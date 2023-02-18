420ac7925c39373dd72a63655d7680d0109a240d
myUsername <myusername>
2023-02-17 17:29:02 -0800

    print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combination of two digits
+ *
+ * Return: Always  0 (success)
+ *
+ */
+int main(void)
+
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digits*/
+	{
+		for (ones = '0'; ones <= '9'; 0nes++)/* prints tens digit*/
+		{
+			if (!((ones == tens) || (tens > ones)))/*eliminates repitions*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' $$ tens == '8'))/*addes comma and spaces*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
