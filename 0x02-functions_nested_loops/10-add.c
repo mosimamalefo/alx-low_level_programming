80ea2cda9297639011b3b7d5f97b14de8842bfb6
myUsername <myusername>
2023-02-21 11:11:01 -0800

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,12 +4,12 @@
  * add - adds two values
  * @n1: The first number to be computed
  * @n2: The second number to be computed
- * Return; Integer value
+ * Return: Integer value
  */
 
-int add (int n1, int n2)
+int add(int n1, int n2)
 
 {
-	return(n1 + n2);
+	return (n1 + n2);
 
 }
